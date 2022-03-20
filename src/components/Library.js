3de032a77d4314278254bc732d88a17e8b199468
Asif Azimi <asifazimi620@gmail.com>
2022-03-20 11:11:38 +0430

    Library changed

--- src/components/Library.js
+++ src/components/Library.js
@@ -10,7 +10,7 @@ const Library = ({
 }) => {
   return (
     <div className={`library ${libraryStatus ? "active-library" : " "}  `}>
-      <h2>Library Song:</h2>
+      <h2>Library`</h2>
       <div className="library-songs">
         {songs.map((song) => (
           <LibrarySong
