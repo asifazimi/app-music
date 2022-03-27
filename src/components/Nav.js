14170d72c6617c01e78e54192ab11bc2021b0bd9
Asif Azimi <asifazimi620@gmail.com>
2022-03-27 21:21:40 +0430

    File Edited

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import { faMusic } from "@fortawesome/free-solid-svg-icons";
 const Nav = ({ setLibraryStatus, libraryStatus }) => {
   return (
     <nav>
-      <h1>Waves</h1>
+      <h1>Waves is: </h1>
       <button
         onClick={() => setLibraryStatus(!libraryStatus)}
         className={libraryStatus ? "library-active" : ""}
